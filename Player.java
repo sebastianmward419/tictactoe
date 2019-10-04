78d0b03f0c89ce2166454213cc8183797f9bdf11
Sebastian Ward <sebastianmward419@gmail.com>
2019-10-04 16:07:41 -0700

    Player class intiialized, with constructor

---
+++ Player.java
@@ -0,0 +1,12 @@
+public class Player {
+    public String name;
+    public String color;
+
+    public Player (String name, String color) {
+        this.name  = name;
+        this.color = color;
+    }
+    public static void main (String[] args) {
+        
+    }
+}
